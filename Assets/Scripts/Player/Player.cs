dfbb54c7d1017a3e43bdf358780900db0f9813ea
Lucas Keven <lucaskeven@ymail.com>
2022-07-28 17:29:42 -0300

    implementing jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {   
     public Rigidbody2D myRigidbody;
+    public Vector2 friction = new Vector2(.1f, 0);
     public Vector2 velocity;
     public float speed;
-
+    public float forceJump = 100;
     void Start()
     {
         
     }
     
     void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    public void HandleMovement()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -24,8 +31,23 @@ public class Player : MonoBehaviour
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(+speed, myRigidbody.velocity.y);
-
-
         }
+
+        //
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+        else if(myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+    }
+
+    public void HandleJump()
+    {
+        if(Input.GetKey(KeyCode.Space))
+            myRigidbody.velocity = Vector2.up * forceJump;
     }
 }
